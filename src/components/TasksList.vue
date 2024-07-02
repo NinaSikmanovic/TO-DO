cdd778f7a3eb4102f62e9f6a5ee9c32f4c6da7eb
Nikolina Sikmanovic <nikolinasikmanovic@nikolinaslaptop.t-com.me>
2024-07-02 16:45:42 +0200

    Add TasksList component

---
+++ src/components/TasksList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    Tasks
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TasksList',
+  created() {
+    this.getTasks();
+  },
+  data: () => ({
+
+  }),
+  methods:  {
+    getTasks(){
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
