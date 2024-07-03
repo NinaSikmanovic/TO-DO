da1be8cd73e8a404f112098e593e1d8c3f646213
Nikolina Sikmanovic <nikolinasikmanovic@nikolinaslaptop.t-com.me>
2024-07-03 16:33:39 +0200

    Code refactoring

--- src/main.js
+++ src/main.js
@@ -1,29 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';  // Import Vuetify CSS
-import './validations/validations';
 import store from "../store";
+import vuetify from './plugins/vuetify';
+import './validations/validations';
 
-Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
-
-const vuetify = new Vuetify({
-  theme: {
-    themes: {
-      light: {
-        primary: '#1976D2',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107',
-      },
-    },
-  },
-});
 
 new Vue({
   vuetify,
